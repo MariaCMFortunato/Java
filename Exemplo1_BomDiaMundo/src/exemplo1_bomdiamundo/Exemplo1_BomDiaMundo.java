33eb525ec9ecf1aa21e109c3ce77a89d820f1a75
Maria <conceicao.martinho.estudo@gmail.com>
2023-10-07 20:15:36 +0100

    Atualização
    Atualização 20231007

---
+++ Exemplo1_BomDiaMundo/src/exemplo1_bomdiamundo/Exemplo1_BomDiaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo1_bomdiamundo;
+
+/**
+ *
+ * @author Maria11
+ */
+public class Exemplo1_BomDiaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here R$otina principal
+        System.out.println("Olá mundo!");
+        System.out.println("Já nos livrámos da maldição. UFA!");
+        System.out.println("Segunda-feira, ai que bom ... !");
+    }
+    
+}
---
+++ TesteNB19JDK20/src/testenb19jdk20/TesteNB19JDK20.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testenb19jdk20;
+
+/**
+ *
+ * @author Maria11
+ */
+public class TesteNB19JDK20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic heresout
+        System.out.println("Olá Mundo!");
+        System.out.println("Consegui!");
+    }
+    
+}
