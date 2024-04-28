2dcda5943ab3d25c4d376810674762cc330f4ecb
Maria <conceicao.martinho.estudo@gmail.com>
2024-04-28 22:16:40 +0100

    Alterações
    Alterações acumuladas

---
+++ ClinicaVeterinaria/src/clinicaveterinaria/ClinicaVeterinaria.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clinicaveterinaria;
+
+/**
+ *
+ * @author Maria11
+ */
+public class ClinicaVeterinaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameMenu jm1= new JFrameMenu();
+        jm1.setVisible(true);
+        
+    }
+    
+}
